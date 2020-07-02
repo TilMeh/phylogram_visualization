a320efdadd27f5c8c94c5c65318d36d2eeb674e2
Tilman Mehl <github@tilman-mehl.de>
2020-07-02 17:03:42 +0200

    replaced custom family lookup with NCBITaxa by ete. Added argument parsing

--- annotate_family_tree.py
+++ annotate_family_tree.py
@@ -1,48 +1,30 @@
+#####################
+# IMPORT OPERATIONS #
+#####################
 from collections import Counter
 import pandas as pd
 import sys
 import subprocess
 import xml.etree.ElementTree as ET
-from ete3 import Tree, TreeStyle, TextFace
+import argparse
+import coloredlogs, logging
+from ete3 import Tree, TreeStyle, TextFace, NCBITaxa
+
+###############
+# AUTHOR INFO #
+###############
+__author__ = 'Michael Gruenstaeudl <m.gruenstaeudl@fu-berlin.de>, '\
+			 'Tilman Mehl <tilmanmehl@zedat.fu-berlin.de>'
+__copyright__ = 'Copyright (C) 2020 Michael Gruenstaeudl and Tilman Mehl'
+__info__ = 'Prune angiosperm phylogenetic tree to family level and annotate ' \
+		   'leaves with counts of avaible plastid genome entries in ' \
+           'NCBI GenBank.'
+__version__ = '2020.07.02.1600'
 
 
-class Lineage_Lookup:
-
-    def __init__(self, fp_dump):
-        print("Intializing lineage lookup...")
-        self.lin_table = self.read_lineage_table(fp_dump)
-
-    def read_lineage_table(self, fp_dump):
-        '''
-        Reads the fullnamelineage.dmp file that is part of the "new_taxdump" archive provided by NCBI at ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump
-        Params:
-         - fp_dump: file path to the .dmp file
-        Returns:
-         - lin_table: pandas DataFrame with columns SPECIES (index) and LINEAGE
-        '''
-        rem_tabs = lambda x : (x.replace("\t", "").strip())
-        lin_table = pd.read_csv(fp_dump, sep="|", header=None, usecols=[1,2], names=["SPECIES", "LINEAGE"], converters = {"SPECIES": rem_tabs, "LINEAGE": rem_tabs}, index_col="SPECIES")
-        return lin_table
-
-    def reduce_to_spermatophyta(self):
-        self.lin_table = self.lin_table.loc[self.lin_table["LINEAGE"].str.find("Spermatophyta") != -1]
-
-    def get_family_name(self, species_name):
-        '''
-        Returns the taxonomic family name of a provided species name.
-        Params:
-         - species_name: str. scientific name of a species
-        Returns:
-         - family_name: str or None. family name of species_name
-        '''
-        family_name = None
-        if species_name in self.lin_table.index:
-            lin = self.lin_table.at[species_name, "LINEAGE"]
-            for rank in lin.split(";"):
-                if rank.endswith("ceae"):
-                    family_name = rank.strip()
-        return family_name
-
+#############
+# FUNCTIONS #
+#############
 
 def get_species_list_from_table(fp_table):
     '''
@@ -54,84 +36,53 @@ def get_species_list_from_table(fp_table):
     species_list = plastome_table["ORGANISM"].to_numpy()
     return species_list
 
-"""
-def add_genome_counts(tree, species_list):
-    '''
-    Adds the number of sequenced plastid genomes per species to tree leaves.
-    Params:
-     - tree: The tree to edit
-     - species_list: a pandas table containing plastome information
-    '''
-    species_list = plastome_table["ORGANISM"].to_numpy()
-    counts_per_organism = Counter(species_list)
-    for organism in counts_per_organism:
-        for leaf in tree.get_leaves_by_name(organism.replace(" ", "_")):
-            leaf.add_feature("sequenced_plastid_genomes", counts_per_organism[organism])
-"""
 
-def get_counts_per_family(species_list, species_in_family):
+def get_genome_count_per_family(species_list, species_in_family):
     '''
     Returns a dict with key = plant family name, value = number of sequenced plastid genomes for that family
     Params:
      - species_list: list of species names. One entry per sequenced plastid genome
      - species_in_family: dict with key=family name, value=set of species names
     '''
-    counts_per_family = {}
+    genome_count_per_family = {}
 
-    counts_per_species = Counter(species_list)
+    genome_count_per_species = Counter(species_list)
 
-    for spec in counts_per_species:
+    for spec in genome_count_per_species:
         for family in species_in_family:
-            for species in species_in_family[family]:
-                if species == spec:
-                    if family in counts_per_family:
-                        counts_per_family[family] += counts_per_species[spec]
-                    else:
-                        counts_per_family[family] = counts_per_species[spec]
+            if spec in species_in_family[family]:
+                if family in genome_count_per_family:
+                    genome_count_per_family[family] += genome_count_per_species[spec]
+                else:
+                    genome_count_per_family[family] = genome_count_per_species[spec]
 
-    return counts_per_family
+    return genome_count_per_family
 
-"""
-TM: turns out querying thousands of taxonomic entries takes a long while. Replaced this with a localized solution
-def get_family_name(species_name):
+
+def get_family_from_lineage(lineage):
     '''
-    Returns the family name of a given species by querying GenBank.
+    Returns the family (taxonomic rank) name from a lineage
     Params:
-     - species_name: the scientific name of a plant species
+     - lineage: dict with NCBI tax id as key and corresponding taxonomic name as value
     '''
-    family_name = None
+    f_name = None
+    for taxname in lineage.values():
+        if taxname.endswith("ceae"):
+            f_name = taxname
+    return f_name
 
-    species_name = species_name.replace("_", " ")
 
-    esearchargs = ["esearch", "-db", "taxonomy", "-query", species_name]
-    esearch = subprocess.Popen(esearchargs, stdout=subprocess.PIPE)
-    efetchargs = ["efetch", "-mode", "xml"]
-    efetch = subprocess.Popen(efetchargs, stdin=esearch.stdout, stdout=subprocess.PIPE)
-    out, err = efetch.communicate()
-
-    try:
-        root = ET.fromstring(out)
-        lineage = root.find("Taxon").find("LineageEx")
-        for taxon in lineage.findall("Taxon"):
-            if taxon.find("Rank").text == "family":
-                family_name = taxon.find("ScientificName").text
-                break
-    except Exception as err:
-        print("Couldn't find family name for species: " + str(species_name) + "\n" + str(err))
-
-    return family_name
-"""
-
-def get_species_in_family(species_list, lineage_lookup):
+def get_species_in_family(species_set, ncbi_db):
     '''
-    Constructs a dictionary from a list of species names where each family name key has a set of species name values assigned.
+    Constructs a dictionary from a set of species names where each family name key has a set of species name values assigned.
     Params:
-     - species_list: the list of species to assign to families
+     - species_set: the set of species to assign to families
+     - ncbi_db: a NCBITaxa object to lookup a species' family name
     '''
-    print("Executing get_species_in_family")
     species_in_family = {}
-    for species in species_list:
-        f_name = lineage_lookup.get_family_name(species)
+    species_taxids = ncbi_db.get_name_translator(species_set)
+    for species, taxid in species_taxids.items():
+        f_name = get_family_from_lineage(ncbi.get_taxid_translator(ncbi.get_lineage(taxid[0])))
         if f_name:
             if f_name in species_in_family:
                 species_in_family[f_name].add(species)
@@ -166,23 +117,38 @@ def prune_to_family(tree, species_in_family):
     tree.prune([f_node.name for f_node in fam_nodes])
 
 
-def attach_counts_to_tree(tree, counts_per_family):
+def get_species_count_per_family(species_in_family):
+    '''
+    Returns the number of species in each family
+    Params:
+     - species_in_family: dict where each family name key has a set of species name values assigned
+    Returns:
+     -  count_species_in_family: a dict where each family name key has the number of species in it assigned
+    '''
+    count_species_in_family = {}
+    for k,v in species_in_family.items():
+        count_species_in_family[k] = len(v)
+    return count_species_in_family
+
+
+def attach_counts_to_tree(tree, genome_count_per_family, species_count_per_family):
     '''
     Attaches TextFaces to tree leaves with plastid genome counts per leaf (=family) and leaf name
     Params:
      - tree: ete3 tree with family names as leaves
-     - counts_per_family: dict with key = family name, value = number of sequenced plastid genomes for that family
+     - genome_count_per_family: dict with key = family name, value = number of sequenced plastid genomes for that family
     '''
     for leaf in tree.iter_leaves():
         leaf.add_face(TextFace(str(leaf.name)), column=0, position="aligned")
-        if leaf.name in counts_per_family:
-            leaf.add_face(TextFace(str(counts_per_family[leaf.name])), column=1, position="aligned")
+        if leaf.name in genome_count_per_family:
+            leaf.add_face(TextFace("(" + str(species_count_per_family[leaf.name]) + "/" + str(genome_count_per_family[leaf.name]) + ")"), column=1, position="aligned")
         else:
-            leaf.add_face(TextFace("0"), column=1, position="aligned")
+            leaf.add_face(TextFace("(" + str(species_count_per_family[leaf.name]) + "/0)"), column=1, position="aligned")
 
-# For some reason, the leaf nodes still do not align
+
 def extend_leaf_branches(tree):
     '''
+    -- DOES NOT YET WORK AS DESIRED --
     Extends leaf branches so the leaf nodes are aligned with the most distant leaf
     Params:
      - tree: ete3 tree
@@ -192,44 +158,55 @@ def extend_leaf_branches(tree):
         dist = tree.get_distance(leaf)
         leaf.dist += maxDist - dist
 
-def color_node(node, color):
 
-    for child in node.get_children():
-        color_node(child)
+def main(args):
 
-def increment_color(color):
+    # STEP 1: Set up logger
+    log = logging.getLogger(__name__)
+	coloredlogs.install(fmt='%(asctime)s [%(levelname)s] %(message)s', level='DEBUG', logger=log)
 
+    # STEP 2: Retrieve and/or update localized NCBI Taxonomy database
+    ncbi = NCBITaxa()
+    ncbi.update_taxonomy_database()
 
-# TODO: Automatisiert die neueste fullnamelineage.dmp Datei vom NCBI ftp ziehen?
-# -> ODER in ete3 eingebaute lokale NCBI Taxonomy DB nutzen
+    # STEP 3: Prune species-level tree to family-level
+
+        # Step 3.1 Read tree from input file
+    log.debug("Loading Tree...")
+    t = Tree(args.infn, format=5)
+        # STEP 3.2: Add species names to species_set_from_tree set
+    log.debug("Gathering species(leaf) names...")
+    species_set_from_tree = Set()
+    for leaf in t.iter_leaves():
+        species_set_from_tree.add(leaf.name.replace("_"," "))
+        # STEP 3.3: Assign species to families
+    log.debug("Constructing dict of species in family...")
+    species_in_family = get_species_in_family(species_set_from_tree, ncbi)
+        # STEP 3.4: Prune the tree
+    log.debug("Pruning Tree to family level...")
+    prune_to_family(t, species_in_family)
+
+    # STEP 4: Calculate counts of species per family and plastid genome entries per family and attach them to the tree leaves
+
+        # STEP 4.1: Read plastid genome information from input table
+    species_list_from_table = get_species_list_from_table(args.tablefn)
+        # STEP 4.2: Count plastid genome entries per family
+    log.debug("Counting plastid genome entries per family...")
+    genome_count_per_family = get_genome_count_per_family(species_list_from_table, species_in_family)
+        # STEP 4.3: Attach counts to tree leaves
+    log.debug("Attaching counts to Tree...")
+    attach_counts_to_tree(t, genome_count_per_family, get_species_count_per_family(species_in_family))
+
+    # STEP 5: Set TreeStyle and render tree
+    ts = TreeStyle()
+    ts.mode = "c"
+    print("Rendering Tree...")
+    t.render(args.outfn, w=10000, h=10000, tree_style=ts)
+
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Missing or too many arguments! Usage: some_functions.py [TREE_FILEPATH_IN] [TREE_FILEPATH_OUT] [TABLE_FILEPATH] [LINEAGE_DUMP]")
-    else:
-        species_list_table = get_species_list_from_table(sys.argv[3])
-        species_list_tree = []
-        print("Loading Tree...")
-        t = Tree(sys.argv[1], format=5)
-        print("Gathering species(leaf) names...")
-        for leaf in t.iter_leaves():
-            species_list_tree.append(leaf.name.replace("_"," "))
-        print("Constructing lineage lookup...")
-        lin_lookup = Lineage_Lookup(sys.argv[4])
-        print("Reducing lineage lookup to Spermatophyta...")
-        lin_lookup.reduce_to_spermatophyta()
-        print("Constructing dict of species in family...")
-        species_in_family = get_species_in_family(species_list_tree, lin_lookup)
-        print("Counting species per family...")
-        counts_per_family = get_counts_per_family(species_list_table, species_in_family)
-        print("Pruning Tree to family level...")
-        prune_to_family(t, species_in_family)
-        print("Attaching counts to Tree...")
-        attach_counts_to_tree(t, counts_per_family)
-        print("Extending leaf branches...")
-        extend_leaf_branches(t)
-
-        ts = TreeStyle()
-        ts.mode = "c"
-        print("Rendering Tree...")
-        t.render(sys.argv[2], w=10000, h=10000, tree_style=ts)
-        #t.write(features=["sequenced_plastid_genomes"], outfile=sys.argv[2], format=5) # Keine Ahnung warum das Feature nicht mitgeschrieben wird
+    parser = argparse.ArgumentParser(description="  --  ".join([__author__, __copyright__, __info__, __version__]))
+    parser.add_argument("-i", "--infn", type=str, required=True, help="path to input tree file. File has to be in Newick format 5 as specified in http://etetoolkit.org/docs/latest/tutorial/tutorial_trees.html#reading-and-writing-newick-trees")
+    parser.add_argument("-o", "--outfn", type=str, required=True, help="path to .png output file")
+    parser.add_argument("-t", "--tablefn", type=str, required=True, help="path to .csv file containing information on plastid genomes available in NCBI GenBank")
+    args = parser.parse_args()
+	main(args)
